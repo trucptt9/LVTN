edd1e466a2eb557da546bad5416f6fc00583153b
Lâm Thành Trung <trunglt@nxcloud.vn>
2023-10-12 09:22:07 +0700

    trunglt update template

--- s3pos/routes/web.php
+++ s3pos/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\AreaController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -14,16 +15,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/login',[UserController::class,'login'])->name('user.login');
-Route::get('/',[UserController::class,'dashboard'])->name('user.dashboard');
 
-Route::prefix('brand')->group(function () {
-   
-    Route::get('detail',[UserController::class,'detail'])->name('brand.detail');
-    
-});
-Route::prefix('stores')->group(function () {
-    Route::get('/',[StoreController::class,'show'])->name('store.all');
-    Route::get('/table',[StoreController::class,'table'])->name('store.table');
-    
-});
+
+Route::get('/login', [UserController::class, 'login'])->name('user.login');
+Route::get('/', [UserController::class, 'dashboard'])->name('user.dashboard');
+
+Route::get('/stores', [AreaController::class, 'show'])->name('user.stores');
+
+Route::get('/admin-index', [HomeController::class, 'index']);
